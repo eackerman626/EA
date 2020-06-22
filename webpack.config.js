ca9658092335343e63a08a96b2507d140442fd3d
Emily Ackerman <emily.ackerman626@gmail.com>
2020-06-22 10:41:33 -0500

    attempt at deploying #1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
 	entry: ['@babel/polyfill', './client/index.js'],
-	mode: 'development',
+	mode: 'production',
 	devServer: {
 		contentBase: path.join(__dirname, 'public'),
 		compress: true,
